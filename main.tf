9cc20c48197b71a29c97a88c91e8f9c740cdcb88
aleksandrandreichenko <andreichenko@web.de>
2021-03-17 22:33:04 +0300

    added IAM access

--- main.tf
+++ main.tf
@@ -12,18 +12,22 @@ resource "aws_iam_instance_profile" "s3_access_profile" {
   role = "${aws_iam_role.s3_access_role.name}"
 }
 
-resource "aws_iam_group_policy" "s3_access_policy" {
+resource "aws_iam_role_policy" "s3_access_policy" {
   name = "s3_access_policy"
   role = "${aws_iam_role.s3_access_role.id}"
-  policy = <<EOF {
+
+  policy = <<EOF
+{
   "Version": "2012-10-17",
-  "Statement": [{
+  "Statement": [
+    {
       "Effect": "Allow",
       "Action": "s3:*",
       "Resource": "*"
     }
-]
-} EOF
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role" "s3_access_role" {
@@ -45,4 +49,5 @@ resource "aws_iam_role" "s3_access_role" {
 }
 EOF
 }
+
 
