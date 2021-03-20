1972cc0e26368fd18adaefff3871eb247e74c5fb
aleksandrandreichenko <andreichenko@web.de>
2021-03-20 19:43:34 +0300

    rework all configuration

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version    =    ">= 0.12.0"
+  backend "s3" {
+    region    = "us-east-1"
+    profile   = "devtest_env"
+    key       = "terraform-state-file"
+    bucket    = "terraform-state-bucket6655"
+  }
+}
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_profile = "devtest_env"
-aws_region = "eu-central-1"
-vpc_cidr = "10.0.0.0/16"
