1444e7ceabf5a9c3eeecaa04866a718af61db826
aleksandrandreichenko <andreichenko@web.de>
2021-03-21 01:37:46 +0300

    rework all configuration

--- network.tf
+++ network.tf
@@ -54,6 +54,12 @@ resource "aws_internet_gateway" "internet-gateway-worker" {
 data "aws_availability_zones" "azs" {
   provider = aws.region-common
   state = "available"
+  tags    = {
+    Name                = "Common Azs"
+    Owner               = "Aleksandr Andreichenko"
+    Environmet          = "Dev-Test"
+    Region              = "us-east-1"
+  }
 }
 
 #Create subnet for common VPC
@@ -62,4 +68,10 @@ resource "aws_subnet" "common_subnet" {
   cidr_block = "10.0.1.0/24"
   availability_zone = element(data.aws_availability_zones.azs.names, 0)
   vpc_id = aws_vpc.vpc_common.id
+  tags    = {
+    Name                = "Common subnet"
+    Owner               = "Aleksandr Andreichenko"
+    Environmet          = "Dev-Test"
+    Region              = "us-east-1"
+  }
 }
