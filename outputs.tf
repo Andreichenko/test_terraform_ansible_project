17cb4a6ec3a9992e708dd3cd4f0a5e773d66a349
aleksandrandreichenko <andreichenko@web.de>
2021-03-21 02:03:12 +0300

    rework all configuration

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,23 @@
 output "aws_worker_subnet" {
   value = aws_subnet.worker_subnet.id
 }
+
+output "aws_common_subnet_primary" {
+  value = aws_subnet.common_subnet_primary.id
+}
+
+output "aws_common_subnet_secondary" {
+  value = aws_subnet.common_subnet_secondary.id
+}
+
+output "internet-gateway-common" {
+  value = aws_internet_gateway.internet-gateway-common.id
+}
+
+output "internet-gateway-worker" {
+  value = aws_internet_gateway.internet-gateway-worker.id
+}
+
+output "vpc_common" {
+  value = aws_vpc.vpc_common.cidr_block
+}
