a3cc8c811e2e3528e7a27453d72925b27e478540
aleksandrandreichenko <andreichenko@web.de>
2021-03-22 17:54:10 +0300

    ELB SG for Jenkins sg

--- security_group.tf
+++ security_group.tf
@@ -4,24 +4,30 @@ resource "aws_security_group" "elb-sg" {
   name = "ELB-SG"
   description = "Allow 443 and traffic to jenkins sg"
   vpc_id = aws_vpc.vpc_common.id
-  ingress {
-    from_port = 443
-    protocol = "tcp"
-    to_port = 443
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow 443 from anywhere"
+
+  dynamic "ingress" {
+    for_each = ["443","80"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      description = "Allow 80,443 from anywhere for redirection"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
-  ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow 80 from anywhere for redirection"
-  }
+
   egress {
     from_port = 0
     protocol = "-1"
     to_port = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags    = {
+    Name                = "Common ELB SG for jenkins"
+    Owner               = "Aleksandr Andreichenko"
+    Environmet          = "Dev-Test"
+    Region              = "us-east-1"
+  }
 }
+
+#Create SG for allowing TCP/8080 from all and tcp/22 from some ip in us-east-1
