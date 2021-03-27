7890f93f0edc77fca7dbf642ba0b098d6f268f8c
aleksandrandreichenko <andreichenko@web.de>
2021-03-27 17:12:25 +0300

    ELB SG for Jenkins sg

--- security_group.tf
+++ security_group.tf
@@ -55,7 +55,7 @@ resource "aws_security_group" "jenkins-sg" {
     from_port = 0
     protocol = "-1"
     to_port = 0
-    cidr_blocks = ["192.168.1.0/24"]
+    cidr_blocks = ["192.168.0.0/24"]
   }
   egress {
     from_port = 0
