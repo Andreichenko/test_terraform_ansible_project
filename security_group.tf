8cb0a151d36d6b411b571fe0ebf62bfc364265c1
aleksandrandreichenko <andreichenko@web.de>
2021-03-28 16:36:59 +0300

    ELB SG for Jenkins sg

--- security_group.tf
+++ security_group.tf
@@ -1,38 +1,37 @@
-#Create SG for LB, only for TCP/80,443 and outband access
+#Create SG for LB, only for TCP/80,443 and outbound access
 resource "aws_security_group" "elb-sg" {
-  provider = aws.region-common
-  name = "ELB-SG"
-  description = "Allow 443 and traffic to jenkins sg"
-  vpc_id = aws_vpc.vpc_common.id
+  provider                 = aws.region-common
+  name                     = "ELB-SG"
+  description              = "Allow 443 and traffic to jenkins sg"
+  vpc_id                   = aws_vpc.vpc_common.id
 
   dynamic "ingress" {
-    for_each = ["443","80"]
+    for_each               = ["443","80"]
     content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
-      description = "Allow 80,443 from anywhere for redirection"
-      cidr_blocks = ["0.0.0.0/0"]
+      from_port            = ingress.value
+      to_port              = ingress.value
+      protocol             = "tcp"
+      description          = "Allow 80,443 from anywhere for redirection"
+      cidr_blocks          = ["0.0.0.0/0"]
     }
   }
-
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port              = 0
+    protocol               = "-1"
+    to_port                = 0
+    cidr_blocks            = ["0.0.0.0/0"]
   }
   tags    = {
-    Name                = "Common ELB SG for jenkins"
-    Owner               = "Aleksandr Andreichenko"
-    Environmet          = "Dev-Test"
-    Region              = "us-east-1"
+    Name                   = "Common ELB SG for jenkins"
+    Owner                  = "Aleksandr Andreichenko"
+    Environmet             = "Dev-Test"
+    Region                 = "us-east-1"
   }
 }
 
 #Create SG for allowing TCP/8080 from all and tcp/22 from some ip in us-east-1
 resource "aws_security_group" "jenkins-sg" {
-  provider = aws.region-worker
+  provider = aws.region-common
   name = "jenkins-sg"
   description = "Allow tcp/8080 tcp/22"
   vpc_id = aws_vpc.vpc_common.id
@@ -55,7 +54,7 @@ resource "aws_security_group" "jenkins-sg" {
     from_port = 0
     protocol = "-1"
     to_port = 0
-    cidr_blocks = ["192.168.0.0/24"]
+    cidr_blocks = ["192.168.1.0/24"]
   }
   egress {
     from_port = 0
