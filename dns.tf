c8ff030670ed79095fa37d1d39985b0d0016009c
aleksandrandreichenko <andreichenko@web.de>
2021-03-30 22:34:24 +0300

    dns changes

--- dns.tf
+++ dns.tf
@@ -7,17 +7,25 @@ resource "aws_route53_zone" "dns_name" {
 #create record in hosted zone for acm certificate domain verification
 resource "aws_route53_record" "cert_validation" {
   provider = aws.region-common
-  name = aws_acm_certificate.jenkins-lb-https.domain_validation_options.resource_record_name
-  type = aws_acm_certificate.jenkins-lb-https.domain_validation_options.resource_record_type
-  records = [aws_acm_certificate.jenkins-lb-https.domain_validation_options.resource_record_value]
-  ttl = 60
-  zone_id = aws_route53_zone.dns_name.zone_id
+  for_each = {
+  for val in aws_acm_certificate.jenkins-lb-https.domain_validation_options : val.domain_name => {
+    name   = val.resource_record_name
+    record = val.resource_record_value
+    type   = val.resource_record_type
+  }
+  }
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.dns.zone_id
+
 }
 
 #Create alias record towards ALB from route53
 resource "aws_route53_record" "jenkins" {
   provider = aws.region-common
-  name     = join(".", ["jenkins", aws_route53_zone.dns_name.name])
+  name     = join(".", ["jenkins", data.aws_route53_zone.dns.name])
   type = "A"
   zone_id = aws_route53_zone.dns_name.zone_id
   alias {
