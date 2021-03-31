6cf95ef57971ab789117a5e7141555b01072963e
aleksandrandreichenko <andreichenko@web.de>
2021-03-31 15:52:43 +0300

    changes and fix

--- security_group.tf
+++ security_group.tf
@@ -5,20 +5,20 @@ resource "aws_security_group" "elb-sg" {
   description              = "Allow 443 and traffic to jenkins sg"
   vpc_id                   = aws_vpc.vpc_common.id
 
-  ingress {
-    description = "Allow 443 from anywhere"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = [
+      "443",
+      "80"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      description = "Allow 80,443 from anywhere for redirection"
+      cidr_blocks = [
+        "0.0.0.0/0"]
+    }
   }
-  ingress {
-    description = "Allow 80 from anywhere for redirection"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   egress {
     from_port              = 0
     protocol               = "-1"
